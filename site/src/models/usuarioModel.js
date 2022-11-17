226bab3e489828ae86847b6beb047292d7b0da72
LucasSilvvaLLima <lucas.lima@sptech.school>
2022-11-17 15:45:22 -0300

    comentarios de alteração

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -31,6 +31,19 @@ function cadastrar(nome, email, senha) {
     return database.executar(instrucao);
 }
 
+    // ALTERAR A FUNÇÃO CADASTRAR APENAS UM ANIME POR USUARIO 
+// function cadastrarAnime(nome, email, senha) {
+//     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+//     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+//     //  e na ordem de inserção dos dados.
+//     var instrucao = `
+//         INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+//     `;
+//     console.log("Executando a instrução SQL: \n" + instrucao);
+//     return database.executar(instrucao);
+// }
+
 module.exports = {
     entrar,
     cadastrar,
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -16,6 +16,8 @@ router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
+// CRIAR O ROUTER.POST()
+
 router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
