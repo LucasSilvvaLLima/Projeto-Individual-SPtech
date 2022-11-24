715dd70590cdd5c6ecc562d6a19545cd05a23a25
LucasSilvvaLLima <lucas.lima@sptech.school>
2022-11-24 11:52:58 -0300

    card aviso favorito

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -32,7 +32,6 @@ function cadastrar(nome, email, senha) {
 }
 
 function cadastrarFavorito(favorito, idUsuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", favorito, idUsuario);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
